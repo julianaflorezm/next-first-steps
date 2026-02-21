f1d81e660838e08c1df2492ca0f19e1ffa2f70df
Juliana <julianamaria304@gmail.com>
2026-02-21 17:16:54 -0500

    First steps

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
       <main className="flex flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <span className="text-5xl">Hola mundo!!!</span>
+        <span className="text-5xl">Hola mundo!!</span>
       </main>
   );
 }
